f255c876af3f1cc88cd9c74ca0dc88d91b178adc
kiurow590 <kiurow590@kiurow590-Linux>
2021-12-13 15:45:14 +0100

    Commit : Creation de la classe larme

--- src/gameloop/Main.java
+++ src/gameloop/Main.java
@@ -21,7 +21,7 @@ public class Main {
 		Spider spider = new Spider(new Vector2(0.2, 0.5), RoomInfos.TILE_SIZE.scalarMultiplication(0.4),
 				ImagePaths.SPIDER, 0.01, new Vector2(), 5, 1, 40);
 
-		Fly fly = new Fly(new Vector2(0.8, 0.8), RoomInfos.TILE_SIZE.scalarMultiplication(0.4), ImagePaths.FLY, 0.007,
+		Fly fly = new Fly(new Vector2(0.8, 0.8), RoomInfos.TILE_SIZE.scalarMultiplication(0.4), ImagePaths.FLY, 0,
 				new Vector2(), 2, 1);
 		GameWorld world = new GameWorld(isaac, spider, fly);
 		initializeDisplay();
---
+++ src/gameobjects/Larme.java
@@ -0,0 +1,12 @@
+package gameobjects;
+
+import libraries.Vector2;
+
+public class Larme {
+
+	private int degat;
+	private double speed;
+	private Vector2 portee;
+	
+	
+}
