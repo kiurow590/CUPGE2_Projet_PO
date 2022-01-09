bfb2ba1f9144c5b566b31e98ae84b65fa80fbc64
theo <theo@PC-Theo.lan>
2022-01-09 12:11:15 +0100

    problème de merge , conflit résolut

--- src/gameWorld/rooms/MonsterRoom.java
+++ src/gameWorld/rooms/MonsterRoom.java
@@ -6,6 +6,7 @@ import libraries.StdDraw;
 import libraries.Vector2;
 import resources.RoomInfos;
 import gameobjects.obstacles.Spikes;
+import gameobjects.obstacles.Poop;
 
 /**
  * Room avec des monstres dedans.
@@ -21,7 +22,6 @@ public class MonsterRoom extends Room {
         this.bgColor = StdDraw.PINK;
         initMonster();
         this.lstObjet.add(initObjectGift());
-        this.lsObstacle.add(new Rock(new Vector2(0.5, 0.5), new Vector2(0.1, 0.1)));
 
     }
 
@@ -59,52 +59,7 @@ public class MonsterRoom extends Room {
         affichageViePiece();
         affichageObjets();
     }
-	private static final String MON_TYPE = "MONSTER_ROOM";
-
-	public MonsterRoom(Hero hero, Integer id) {
-		super(hero, id);
-		this.type = MON_TYPE;
-		this.bgColor = StdDraw.PINK;
-		initMonster();
-		this.lstObjet.add(initObjectGift());
-		
-
-	}
-
-	@Override
-	public void updateRoom() {
-		// TODO Auto-generated method stub
-		makeHeroPlay();
-		makeMonsterPlay();
-		collisionReport();
-		rammasseMonstreMort();
-		nettoyageLarme();
-		nettoyageProj();
-	}
-
-	@Override
-	public void drawRoom() {
-		// For every tile, set background color.
-		StdDraw.setPenColor(this.bgColor);
-		for (int i = 0; i < RoomInfos.NB_TILES; i++) {
-			for (int j = 0; j < RoomInfos.NB_TILES; j++) {
-				Vector2 position = positionFromTileIndex(i, j);
-				StdDraw.filledRectangle(position.getX(), position.getY(), RoomInfos.HALF_TILE_SIZE.getX(),
-						RoomInfos.HALF_TILE_SIZE.getY());
-			}
-		}
-
-		this.drawWall();
-
-		hero.drawGameObject();
-		dessinePorte();
-
-		dessineMonstre();
-		dessineLarme();
-		dessineObstacles();
-		affichageViePiece();
-		affichageObjets();
-	}
+	
 
 	public void choixpatternObstacle() {
 		// on choisi un pattern possible parmis les 6 ( construit a la main
--- src/gameWorld/rooms/Room.java
+++ src/gameWorld/rooms/Room.java
@@ -331,8 +331,7 @@ public abstract class Room {
 	 */
 	public void collisionObjet() {
 		collisionObstacle(this.lsObstacle);
-		for (int i = 0; i < lstObjet.size(); i++) {
-
+		
 		// pour chacun de mes objet
 		for (int i = 0; i < lstObjet.size(); i++) {
 			// si je ne suis pas dans une shopRoom
